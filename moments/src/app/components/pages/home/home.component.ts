ddf372e39142d2b4e4597b166cda2f0d3280c5f9
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-08-12 13:54:12 +0100

    finished home layout

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { MomentService } from 'src/app/services/moment.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
-  }
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
 
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
