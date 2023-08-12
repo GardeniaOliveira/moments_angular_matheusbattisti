db48d9ed316b4d9fc604052a38a9a698a686b92e
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-08-12 09:05:19 +0100

    finished msg component and about page

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,35 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Moment } from 'src/app/Moment';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent implements OnInit {
-
   btnText = 'Share!';
-  constructor(private momentService: MomentService) { }
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  async createHandler(moment:Moment){
+  async createHandler(moment: Moment) {
     //recieve data from the moment component
     const formData = new FormData();
 
     formData.append('title', moment.title);
     formData.append('description', moment.description);
 
-    if(moment.image){
+    if (moment.image) {
       formData.append('image', moment.image);
     }
 
-    await this.momentService.createMoment(formData).subscribe();
+    // await this.momentService.createMoment(formData).subscribe();
 
+    // this.messagesService.add('added message sucessfully');
+
+    // //redirect user to the home after add a message
+    // this.router.navigate(['/']);
+
+    this.momentService.createMoment(formData).subscribe({
+      next: () =>{
+        this.messagesService.add("added message sucessfully!");
+        this.router.navigate(['/'])
+      }
+    });
   }
-
 }
